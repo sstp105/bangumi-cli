fd338817f15d7fd8cb520794f02da8a4c2dee5d7
sstp105 <liyang0525.ly@gmail.com>
2025-04-06 19:12:56 -0700

    close #13

--- cmd/unsubscribe.go
+++ cmd/unsubscribe.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/sstp105/bangumi-cli/internal/handler/unsubscribe"
 )
+
+var id int
 
 var unsubscribeCmd = &cobra.Command{
 	Use:   "unsubscribe",
@@ -16,10 +19,12 @@ Summary:
   bangumi unsubscribe --id 3513
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		unsubscribe.Run(id)
 	},
 }
 
 func init() {
+	unsubscribeCmd.Flags().IntP("id", "i", -1, "指定取消订阅的番剧 ID")
+
 	rootCmd.AddCommand(unsubscribeCmd)
 }
--- internal/handler/unsubscribe/unsubscribe.go
+++ internal/handler/unsubscribe/unsubscribe.go
@@ -1,1 +1,81 @@
 package unsubscribe
+
+import (
+	"fmt"
+	"github.com/sstp105/bangumi-cli/internal/console"
+	"github.com/sstp105/bangumi-cli/internal/mikan"
+	"github.com/sstp105/bangumi-cli/internal/path"
+)
+
+func Run(id int) {
+	if id == -1 {
+		removeAll()
+	}
+	remove(id)
+}
+
+func removeAll() {
+	var subscription []mikan.BangumiBase
+	if err := path.ReadJSONConfigFile(path.SubscriptionConfigFile, &subscription); err != nil {
+		console.Errorf("读取本地订阅配置文件错误:%s", err)
+		return
+	}
+
+	var failures []mikan.BangumiBase
+	for _, s := range subscription {
+		fn := s.ConfigFileName()
+		if err := path.DeleteJSONConfigFile(fn); err != nil {
+			console.Warningf("取消订阅%s错误:%s", s.Name, err)
+			failures = append(failures, s)
+		}
+		console.Plainf("已取消订阅:%s", s.Name)
+	}
+
+	if err := path.DeleteJSONConfigFile(path.SubscriptionConfigFile); err != nil {
+		console.Errorf("删除本地订阅配置文件错误:%s", err)
+	}
+
+	if len(failures) == 0 {
+		console.Successf("已成功取消订阅 %d 部番剧", len(subscription))
+		return
+	}
+
+	console.Warningf("以下%d 部番剧取消订阅失败, 请重试 bangumi unsubscribe 或指定 --id", len(failures))
+	for _, b := range failures {
+		console.Plain(b.Name)
+	}
+}
+
+func remove(id int) {
+	var subscription []mikan.BangumiBase
+	if err := path.ReadJSONConfigFile(path.SubscriptionConfigFile, &subscription); err != nil {
+		console.Errorf("读取本地订阅配置文件错误:%s", err)
+		return
+	}
+
+	subscription = removeByID(subscription, id)
+	if err := path.SaveJSONConfigFile(path.SubscriptionConfigFile, subscription); err != nil {
+		console.Errorf("保存本地订阅配置文件错误:%s", err)
+		return
+	}
+
+	fn := fmt.Sprintf("%d.json", id)
+	if err := path.DeleteJSONConfigFile(fn); err != nil {
+		console.Errorf("取消订阅失败:%s", err)
+		return
+	}
+
+	console.Successf("取消订阅 %d 成功", id)
+}
+
+func removeByID(subscription []mikan.BangumiBase, id int) []mikan.BangumiBase {
+	ids := fmt.Sprintf("%d", id)
+
+	var res []mikan.BangumiBase
+	for _, s := range subscription {
+		if s.ID != ids {
+			res = append(res, s)
+		}
+	}
+	return res
+}
